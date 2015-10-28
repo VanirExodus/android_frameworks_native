28b86d614e7136414cc839bb9485dfa0d17536af
Haixia Shi <hshi@google.com>
2015-10-28 16:19:01 -0700

    SF: more DispSync improvements.
    Pass the reference time to DispSyncThread. Since the phase offset is calculated
    using timestamps relative to the reference time, we must also adjust the phase
    offset by the same reference time when computing the next refresh time.
    
    Always reset phase offset to zero when updating the reference time because the
    reference time equals the first timestamp.
    
    After beginResync() we need to keep HW vsync enabled until the model is updated.
    
    Bug: 25113115
    Change-Id: I8eae227bee91c24a99bf8e57fbebceb98d29c77d
    Test: check in systrace that app/sf vsync events have correct phase

--- services/surfaceflinger/DispSync.cpp
+++ services/surfaceflinger/DispSync.cpp
@@ -288,7 +288,6 @@ void DispSync::reset() {
     mModelUpdated = false;
     mNumResyncSamples = 0;
     mFirstResyncSample = 0;
-    mResyncReferenceTime = 0;
     mNumResyncSamplesSincePresent = 0;
     resetErrorLocked();
 }
@@ -330,7 +329,8 @@ bool DispSync::addResyncSample(nsecs_t timestamp) {
     size_t idx = (mFirstResyncSample + mNumResyncSamples) % MAX_RESYNC_SAMPLES;
     mResyncSamples[idx] = timestamp;
     if (mNumResyncSamples == 0) {
-        mResyncReferenceTime = timestamp;
+        mPhase = 0;
+        mReferenceTime = timestamp;
     }
 
     if (mNumResyncSamples < MAX_RESYNC_SAMPLES) {
@@ -409,7 +409,7 @@ void DispSync::updateModelLocked() {
         double scale = 2.0 * M_PI / double(mPeriod);
         for (size_t i = 0; i < mNumResyncSamples; i++) {
             size_t idx = (mFirstResyncSample + i) % MAX_RESYNC_SAMPLES;
-            nsecs_t sample = mResyncSamples[idx] - mResyncReferenceTime;
+            nsecs_t sample = mResyncSamples[idx] - mReferenceTime;
             double samplePhase = double(sample % mPeriod) * scale;
             sampleAvgX += cos(samplePhase);
             sampleAvgY += sin(samplePhase);
@@ -445,7 +445,7 @@ void DispSync::updateErrorLocked() {
     nsecs_t sqErrSum = 0;
 
     for (size_t i = 0; i < NUM_PRESENT_SAMPLES; i++) {
-        nsecs_t sample = mPresentTimes[i] - mResyncReferenceTime;
+        nsecs_t sample = mPresentTimes[i] - mReferenceTime;
         if (sample > mPhase) {
             nsecs_t sampleErr = (sample - mPhase) % period;
             if (sampleErr > period / 2) {
