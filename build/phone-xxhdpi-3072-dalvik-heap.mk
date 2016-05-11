b1e676afe5d87dfe1991544a649ad1ac5a9bcc83
Louis Popi <theh2o64@gmail.com>
2016-05-11 22:48:52 +0200

    Add dalvik heap/hwui overrides for xxhdpi phone with 3072MB RAM
    Change-Id: I0ebf2033341e8f09004c1e2dec5f4438aa52e5dc

--- build/phone-xxhdpi-3072-dalvik-heap.mk
+++ build/phone-xxhdpi-3072-dalvik-heap.mk
@@ -17,9 +17,9 @@
 # Provides overrides to configure the Dalvik heap for a 3G phone
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=24m \
-    dalvik.vm.heapgrowthlimit=320m \
-    dalvik.vm.heapsize=768m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
